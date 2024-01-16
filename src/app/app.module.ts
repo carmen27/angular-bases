2dca4876e63ad42017598b6907307e439f4ff645
Cristina Osorio <cristinaov87@gmail.com>
2024-01-16 15:42:10 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/components/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -14,8 +14,9 @@ import { HeroesModule } from './heroes/heroes.module';
   ],
   imports: [
     BrowserModule,
-    HeroesModule , 
-    CounterModule 
+    HeroesModule ,
+    CounterModule ,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
